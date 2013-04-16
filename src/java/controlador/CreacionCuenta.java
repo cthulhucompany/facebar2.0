90388632f98350dbf88b2121bd5bbb6bd23d440d
yovas <yovas@yovas-netbook>
2013-04-16 05:05:40 -0500

    Perfeccionamiento

--- src/java/controlador/CreacionCuenta.java
+++ src/java/controlador/CreacionCuenta.java
@@ -6,14 +6,6 @@ package controlador;
 
 import java.io.IOException;
 import java.sql.SQLException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import modelo.ConexionBD;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -23,6 +15,13 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import modelo.ConexionBD;
 
 /**
  *
